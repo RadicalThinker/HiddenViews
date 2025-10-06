cc4d513a077c622fba9ee04d210fff71dce0f10d
radicalthinker <undergod.code@gmail.com>
2025-10-06 22:25:22 +0530

    updated resend

--- src/helpers/sendVerificationEmail.ts
+++ src/helpers/sendVerificationEmail.ts
@@ -12,7 +12,7 @@ export async function sendVerificationEmail(
     logger.debug('Sending verification email', { email, username });
     
     const response = await resend.emails.send({
-      from: "Acme <onboarding@resend.dev>",
+      from: "Acme <voicesecret9@gmail.com>",
       to: email,
       subject: "HiddenViews | Verification Code",
       react: VerificationEmail({ username, otp: verifyCode }),
